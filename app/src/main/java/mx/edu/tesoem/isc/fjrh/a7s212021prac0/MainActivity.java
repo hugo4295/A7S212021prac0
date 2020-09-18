996a9bfd6c57512f6409df190e3348a0d954f524
Victor Hugo De la O Martínez <hugo_4295@hotmail.com>
2020-09-17 21:05:21 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/isc/fjrh/a7s212021prac0/MainActivity.java
@@ -0,0 +1,25 @@
+package mx.edu.tesoem.isc.fjrh.a7s212021prac0;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText txtnom;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtnom = findViewById(R.id.txtnombre);
+    }
+
+    public void saluda(View v){
+        Toast.makeText(this,"Bienvenido " + txtnom.getText(),Toast.LENGTH_LONG).show();
+    }
+}
